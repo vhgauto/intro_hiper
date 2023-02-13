024c5647ae13dca16fed9ea600dcdcf04d11fe83
vhgauto <victor.gauto@outlook.com>
2023-02-13 12:41:10 -0300

    Agrego carpetas

---
+++ intro_hiper.R
@@ -0,0 +1,10 @@
+
+# paquetes ----------------------------------------------------------------
+
+library(prismaread)
+library(tidyverse)
+library(raster)
+
+# vector ------------------------------------------------------------------
+
+puntos <- shapefile("vector/puntos.shp")
